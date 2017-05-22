f0860cbdf530f1d03554c439126cc9203826bd58
samallenqing <samallenqing@gmail.com>
2017-05-21 23:20:36 -0700

    Add files via upload

---
+++ GladLib/GladLibMap.java
@@ -0,0 +1,126 @@
+
+/**
+ * Write a description of GladLibMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.*;
+
+public class GladLibMap {
+    
+    private HashMap<String, ArrayList<String>> myMap;
+    private Random myRandom;
+    private ArrayList<String> countwords;
+    private static String dataSourceURL = "http://dukelearntoprogram.com/course3/data";
+    private static String dataSourceDirectory = "/Users/qinqing/Desktop/Java/GladLib/data";
+    
+    public GladLibMap(){
+        initializeFromSource(dataSourceDirectory);
+        myMap = new HashMap<String, ArrayList<String>>();
+        myRandom = new Random();
+        countwords = new ArrayList();
+    }
+    
+    public GladLibMap(String source){
+        initializeFromSource(source);
+        myRandom = new Random();
+    }
+    
+    private void initializeFromSource(String source) {
+        String [] lables = {"adjective", "noun", "color", "country", "name", "animal", "time","verb","fruit"};
+          for(String s: lables){
+            ArrayList<String> list = readIt(source+"/"+s+".txt");
+            myMap.put(s, list);
+        }
+    }
+    
+    private String randomFrom(ArrayList<String> source){
+        int index = myRandom.nextInt(source.size());
+        return source.get(index);
+    }
+    
+    private String getSubstitute(String label) {
+        if(myMap.containsKey(label)){
+            return randomFrom(myMap.get(label));
+        }
+        return "**UNKNOWN**";
+    }
+    
+    private String processWord(String w){
+        int first = w.indexOf("<");
+        int last = w.indexOf(">",first);
+        if (first == -1 || last == -1){
+            return w;
+        }
+        String prefix = w.substring(0,first);
+        String suffix = w.substring(last+1);
+        String sub = getSubstitute(w.substring(first+1,last));
+        if(countwords.contains(sub) == true){
+            sub = getSubstitute(w.substring(first+1,last));
+          }
+        else{
+            countwords.add(sub);
+          }
+        return prefix+sub+suffix;
+    }
+    
+    
+    
+    private String fromTemplate(String source){
+        String story = "";
+        if (source.startsWith("http")) {
+            URLResource resource = new URLResource(source);
+            for(String word : resource.words()){
+                story = story + processWord(word) + " ";
+            }
+        }
+        else {
+            FileResource resource = new FileResource(source);
+            for(String word : resource.words()){
+                story = story + processWord(word) + " ";
+            }
+        }
+        return story;
+    }
+    
+    private ArrayList<String> readIt(String source){
+        ArrayList<String> list = new ArrayList<String>();
+        if (source.startsWith("http")) {
+            URLResource resource = new URLResource(source);
+            for(String line : resource.lines()){
+                list.add(line);
+            }
+        }
+        else {
+            FileResource resource = new FileResource(source);
+            for(String line : resource.lines()){
+                list.add(line);
+            }
+        }
+        return list;
+    }
+    
+    public void makeStory(){
+        System.out.println("\n");
+        String story = fromTemplate("data/madtemplate2.txt");
+        printOut(story, 70);
+    }
+    
+    
+    private void printOut(String s, int lineWidth){
+        int charsWritten = 0;
+        for(String w : s.split("\\s+")){
+            if (charsWritten + w.length() > lineWidth){
+                System.out.println();
+                charsWritten = 0;
+            }
+            System.out.print(w +" ");
+            charsWritten += w.length() + 1;
+        }
+    }
+    
+
+
+}
